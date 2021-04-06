e04960e29c88923bc8cd85a072c1f6ab80491a0a
Tatsuya Maeta <tat.mae084@gmail.com>
2021-04-06 10:32:02 +0900

    fixed files

--- js/other_auth/firebase.js
+++ js/other_auth/firebase.js
@@ -5,6 +5,9 @@ var firebaseConfig = envFirebaseConfig; //envFirebaseConfigは別ディレクト
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// firebase.auth()のメソッド一覧
+// https://firebase.google.com/docs/reference/js/firebase.auth.Auth
+
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
@@ -14,13 +17,14 @@ firebase.auth().onAuthStateChanged(function (user) {
         document.getElementById("login_div").style.display = "none";
 
         var user = firebase.auth().currentUser;
-        
+
         console.dir(user);
 
         if (user != null) {
             var email_id = user.email;
             document.getElementById("user_para").innerHTML =
                 "Welcome User : " + email_id;
+            
         }
     } else {
         // No user is signed in.
@@ -39,12 +43,13 @@ function login() {
         .signInWithEmailAndPassword(userEmail, userPass)
         .then(console.log(userEmail, userPass))
         .catch(function (error) {
-            // Handle Errors here.
+
+            var e = checkErrorDetail(error.code);
+            // console.log(e);
             var errorCode = error.code;
             var errorMessage = error.message;
-            console.dir(error);
-            window.alert("Error : " + errorMessage);
-            // ...
+            // console.dir(error);
+            window.alert("エラー : " + e);
         });
 }
 
@@ -69,8 +74,53 @@ document
                 document.getElementById("password_field").value != ""
             ) {
                 console.log("Pressed Double Key with command key.");
-                
+
                 login();
             }
         }
     });
+
+function checkErrorDetail(error) {
+    switch (error) {
+        case "auth/cancelled-popup-request":
+        case "auth/popup-closed-by-user":
+            return null;
+        case "auth/email-already-in-use":
+            if (method.indexOf("signup") !== -1) {
+                return "このメールアドレスは使用されています";
+            } else {
+                return "メールアドレスまたはパスワードが違います";
+            }
+        case "auth/invalid-email":
+            return "メールアドレスが正しくありません";
+        case "auth/user-disabled":
+            return "サービスの利用が停止されています";
+        case "auth/user-not-found":
+            return "メールアドレスまたはパスワードが違います";
+        case "auth/user-mismatch":
+            if (method === "signin/popup") {
+                return "認証されているユーザーと異なるアカウントが選択されました";
+            } else {
+                return "メールアドレスまたはパスワードが違います";
+            }
+        case "auth/weak-password":
+            return "パスワードは6文字以上にしてください";
+        case "auth/wrong-password":
+            return "メールアドレスまたはパスワードが違います";
+        case "auth/popup-blocked":
+            return "認証ポップアップがブロックされました。ポップアップブロックをご利用の場合は設定を解除してください";
+        case "auth/operation-not-supported-in-this-environment":
+        case "auth/auth-domain-config-required":
+        case "auth/operation-not-allowed":
+        case "auth/unauthorized-domain":
+            return "現在この認証方法はご利用頂けません";
+        case "auth/requires-recent-login":
+            return "認証の有効期限が切れています";
+        default:
+            if (method.indexOf("signin") !== -1) {
+                return "認証に失敗しました。しばらく時間をおいて再度お試しください";
+            } else {
+                return "エラーが発生しました。しばらく時間をおいてお試しください";
+            }
+    }
+}
